2a3ecdc889a8fa0fe516e908c87d21a8e2b02d77
ManSenAsh . <twistter.2240@gmail.com>
2025-02-27 10:22:30 -0600

    Agregar archivos de proyecto.

---
+++ rainbow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rainbow
+{
+    public class reo 
+    {
+        public double vectores(double x1, double y2, double x2, double y1)
+        {
+            return x1 - y2 + x2 - y1;
+                
+        }
+    
+    }
+    public class reo2
+    {
+        public double vectores(double x1, double x2, double x3)
+        {
+            return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(x2, 2)+ Math.Pow(x3, 2));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            reo operacion1 = new reo();
+            reo2 operacion2 = new reo2();
+            Console.WriteLine("usar poliformismo con vectores de 2 y 3 dimensiones");
+            Console.WriteLine("operacion 1");
+            Console.WriteLine(operacion1.vectores(12.2, 15.2, 12.3, 12.9));
+            Console.WriteLine("operacion 2");
+            Console.WriteLine(operacion2.vectores(15.12, 15.6, 15.2));
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ rainbow/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("rainbow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rainbow")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e1a855d8-2665-4fdc-af7d-0c93ee941c38")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
